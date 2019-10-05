18370fc09268623b2a9d8c1b276e97c0fb81eaf6
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-10-05 15:09:53 +0000

    ES6 features used in this exercise

--- src/App.js
+++ src/App.js
@@ -106,6 +106,18 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<ul>
+			{profiles.map((profile) => {
+              	const userName = users[profile.userID].name;
+				const movieName = movies[profile.favoriteMovieID].name;
+
+				return (
+                	<li key={profile.id}>
+                  		<p>{userName}'s favorite movie is "{movieName}."</p>
+              		</li>
+                )
+			})}
+		</ul>
       </div>
     );
   }
